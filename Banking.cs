37bd2560d701db8c2981f378199cbd50c6cb9a35
Dmafuta <dmafuta07@gmail.com>
2026-03-15 17:37:30 +0300

    Banking class

---
+++ Banking.cs
@@ -0,0 +1,31 @@
+﻿namespace SelfStudy
+{
+    public class Banking
+    {
+        User _user;
+        string? _accountnumber;
+        string? _accountname;
+
+        public Banking()
+        {
+            _user = new User();
+            _accountnumber = string.Empty;
+            _accountname = string.Empty;
+        }
+
+        public Banking(User user, string accountnumber, string accountname)
+        {
+            this._user = new User();
+            this._accountnumber = accountnumber;
+            this._accountname = accountname;
+        }
+
+        public void SetAccountUser(User user) { this._user = user; }
+        public string GetAccountUser(string user) { return user; }
+        public void SetAccountName(string accountname) { this._accountname = accountname; }
+        public string? GetAccountName() { return this._accountname; }
+        public void SetAccountNumber(string accounnumber) { this._accountnumber = accounnumber; }
+        public string? GetAccountNumber() { return this._accountnumber; }
+
+    }
+}
